50e99e8dc79b43eda8b3878bd2740e9cd6551015
David Silvera <dav.silvera@gmail.com>
2021-12-16 11:51:26 +0100

    Refactor Star to use one image

--- lib/common/star.dart
+++ lib/common/star.dart
@@ -6,12 +6,13 @@ import 'package:flame/sprite.dart';
 import 'package:spaceshooter/game_manager.dart';
 
 class Star extends SpriteAnimationComponent with HasGameRef<GameManager> {
-
   @override
   Future<void>? onLoad() async {
-    var spriteSheet = SpriteSheet(image: await Images().load("stars_${Random().nextInt(3) + 1}.png"), srcSize: Vector2(9.0, 9.0));
+    var spriteSheet = SpriteSheet(
+        image: await Images().load("stars.png"), srcSize: Vector2(9.0, 9.0));
     animation = spriteSheet.createAnimation(
-        row: 0, stepTime: (Random().nextInt(50) / 10) + 0.2);
+        row: Random().nextInt(3) + 1,
+        stepTime: (Random().nextInt(50) / 10) + 0.2);
     var size = Random().nextInt(10).toDouble() + 10;
     var x = Random()
         .nextInt((gameRef.size.toRect().width - size).toInt())
